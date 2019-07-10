05a9c9e741347e4812ce3ceea67e1457dfedad6b
poignant-minifier-0983 <davidfano@gmail.com>
2019-07-10 01:45:09 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,7 +1,10 @@
-class CreateSongs < ActiveRecord::Migration[4.2]
+
+class CreateArtists < ActiveRecord::Migration[4.2]
   def change
-    create_table :artists do |t|
+    create_table :songs do |t|
       t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
     end
   end
 end
